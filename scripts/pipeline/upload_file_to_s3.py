36c54ba80a7b5949d8f7f0a836044821372dad09
Yeferson Chaverra <ychaverra@sura.com.co>
2023-03-28 14:54:38 -0500

    Realicé cambio de nombre de Bucket

--- scripts/pipeline/upload_file_to_s3.py
+++ scripts/pipeline/upload_file_to_s3.py
@@ -3,19 +3,14 @@ import sys
 
 def main():
     
-    print ("hola")
-    print (sys.argv)
+ 
     bucket_name=sys.argv[1]
     aws_key=sys.argv[2]
     aws_access_key=sys.argv[3]
     aws_access_secret=sys.argv[4]
     local_path=sys.argv[5]
 
-    print (bucket_name)
-    print (aws_key)
-    print (aws_access_key)
-    print (aws_access_secret)
-    print (local_path)
+
 
     session = boto3.Session(
         aws_access_key_id=aws_access_key,
